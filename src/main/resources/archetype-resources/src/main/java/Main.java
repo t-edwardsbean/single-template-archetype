2fa65b8ac567e2ea97da9088be4c63431a0c38ea
edwardsbean@gmail.com <edwardsbean@gmail.com>
2015-04-15 17:44:42 +0800

    first

---
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,14 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+/**
+ * 程序入口
+ * Created by edwardsbean on 2015/4/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
